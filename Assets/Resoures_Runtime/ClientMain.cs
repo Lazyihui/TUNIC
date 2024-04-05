3c5dd692d858e9ba5d80f29b1c0effe20856da5e
包子 <chengbaozi6@qq.com>
2024-04-05 15:52:34 +0800

    <feature> 新项目

---
+++ Assets/Resoures_Runtime/ClientMain.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TUNIC {
+
+    public class ClientMain : MonoBehaviour {
+        void Awake() {
+            Debug.Log("ClientMain Awake");
+        }
+
+        void Update() {
+
+        }
+    }
+}
